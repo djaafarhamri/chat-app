1574fb8700a9a70d4e394c2c3b59c94bac19ef97
djaafarhamri <hamridjaafar12@gmail.com>
2022-03-26 16:52:59 +0100

    setup server.js

---
+++ server.js
@@ -0,0 +1,57 @@
+const express = require("express");
+const app = express();
+const dotenv = require("dotenv");
+dotenv.config();
+const http = require("http");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const server = http.createServer(app);
+const socket = require("socket.io");
+const path = require("path");
+const mongoose = require("mongoose");
+const PORT = process.env.PORT || 4000;
+
+mongoose
+  .connect("mongodb://localhost:27017/chat-app", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("data base connecte");
+    server.listen(PORT, () => {
+      console.log("listening on PORT : ", PORT);
+    });
+  })
+  .catch((err) => {
+    throw new Error(err);
+  });
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", req.get("Origin") || "*");
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,PUT,PATCH,POST,DELETE");
+  res.header("Access-Control-Expose-Headers", "Content-Length");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "X-HTTP-Method-Override, Accept, Authorization, Content-Type, X-Requested-With, Range"
+  );
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  } else {
+    return next();
+  }
+});
+//app.use(cors())
+app.use(cookieParser());
+app.use(express.static("public"));
+app.use(express.json());
+
+const io = socket(server, {
+  cors: {
+    origin: "http://localhost:3000",
+  },
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
