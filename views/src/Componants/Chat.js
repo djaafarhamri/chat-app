1ee7b85e47b1115edd2da9ede7958921c0f514b8
djaafarhamri <hamridjaafar12@gmail.com>
2022-03-27 15:54:02 +0100

    chat input

--- views/src/Componants/Chat.js
+++ views/src/Componants/Chat.js
@@ -3,7 +3,13 @@ import './chat.css';
 const Chat = () => {
     return ( 
         <div className="chat">
-
+            <div className="messages">
+                
+            </div>
+            <div className="input">
+                <input type="text" placeholder='Aa' />
+                <button>Send</button>
+            </div>
         </div>
      );
 }
---
+++ views/src/Componants/Message.js
@@ -0,0 +1,11 @@
+import './message.css'
+
+const Message = () => {
+    return ( 
+        <div className="message">
+
+        </div>
+     );
+}
+ 
+export default Message;
