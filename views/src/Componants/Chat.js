a23971a58ba31e96fadeece7ca05908e972d0e46
djaafarhamri <hamridjaafar12@gmail.com>
2022-03-27 17:36:59 +0100

    messages

--- views/src/Componants/Chat.js
+++ views/src/Componants/Chat.js
@@ -15,7 +15,15 @@ const Chat = () => {
   const messages = [
     {
       text: "Hello 3",
-      type: "sent 3",
+      type: "sent",
+    },
+    {
+      text: "Heyweuncweicmweicmwiec wi cwiencwiej cw cuiq cqwi cqhw ciqw cqiw cqw qwic qwc ",
+      type: "received",
+    },
+    {
+      text: "Hello",
+      type: "received",
     },
     {
       text: "Hey",
@@ -67,15 +75,7 @@ const Chat = () => {
     },
     {
       text: "Hey",
-      type: "received",
-    },
-    {
-      text: "Hello",
       type: "sent",
-    },
-    {
-      text: "Hey",
-      type: "received",
     },
     {
       text: "Hello",
@@ -98,11 +98,16 @@ const Chat = () => {
     <div className="chat">
       <div className="messages-container">
         <div className="messages">
-          {messages.map((message, index) => (
-            <div key={index}>
-              <Message message={message} />
-            </div>
-          ))}
+          {messages.map((message, index) => {
+              if (index + 1 < messages.length) {
+              var reSent = message.type === messages[index + 1].type;
+              }
+            return (
+              <div key={index}>
+                <Message reSent={reSent} message={message} />
+              </div>
+            );
+          })}
         </div>
         <div ref={messagesEndRef} />
       </div>
--- views/src/Componants/Message.js
+++ views/src/Componants/Message.js
@@ -1,13 +1,24 @@
-import image from '../assets/avatar.jpeg';
-import './message.css'
+import image from "../assets/avatar.jpeg";
+import empty from "../assets/empty.png";
+import "./message.css";
 
 const Message = (props) => {
-    return ( 
-        <div className="message">
-            <img src={image} alt="" />
-            <h4>{props.message.text}</h4>
+    // console.log(props);
+  return (
+    <div className="message">
+      {props.message.type === "received" ? (
+        <div className={props.reSent ? "message-received":"message-received-resent"}>
+          {!props.reSent && <img src={image} alt="" />}
+          <h4>{props.message.text}</h4>
         </div>
-     );
-}
- 
+      ) : (
+        <div className={props.reSent ? "message-sent":"message-sent-resent"}>
+          <h4>{props.message.text}</h4>
+          {!props.reSent && <img src={image} alt="" />}
+        </div>
+      )}
+    </div>
+  );
+};
+
 export default Message;
