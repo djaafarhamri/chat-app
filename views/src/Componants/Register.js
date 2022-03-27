2d4249024b4acc84c5140743a459f7ccbcb7ce45
djaafarhamri <hamridjaafar12@gmail.com>
2022-03-27 21:03:46 +0100

    register

--- views/src/Componants/Register.js
+++ views/src/Componants/Register.js
@@ -1,21 +1,34 @@
 import "./register.css";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
+import { useState } from "react";
 
 const Register = () => {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const navigate = useNavigate();
+
+  const sign_up = () => {
+    axios.post("http://localhost:4000/sign_up", { username, email, password })
+      .then(res => { navigate("/sign-in"); })
+      .catch(err => { setError(err.message) });
+  }
   return (
     <div className="register">
       <div className="container" id="container">
         <div className="form-container sign-up-container">
-          <form action="#">
+          <div className="form">
             <h1>Create Account</h1>
-            <input type="text" placeholder="Name" />
-            <input type="email" placeholder="Email" />
-            <input type="password" placeholder="Password" />
-            <button>Sign Up</button>
+            <input type="text" placeholder="Username" onChange={(e) => setUsername(e.target.value)} />
+            <input type="email" placeholder="Email" onChange={(e) => setEmail(e.target.value)} />
+            <input type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} />
+            <button onClick={sign_up}>Sign Up</button>
             <p>
               Already have an account <Link to="/sign-in">Sign in</Link>
             </p>
-          </form>
+          </div>
         </div>
       </div>
     </div>
